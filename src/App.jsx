f80e106e8b716ec0d28b7ec20ab4a5670ee6a9e4
vasudevshetty <vasudeepu2815@gmail.com>
2025-06-30 17:14:22 +0530

    Remove unused components and simplify App structure for a cleaner portfolio site

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import ScrollToTop from "./components/ScrollToTop";
-import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Skills from "./sections/Skills";
-import Projects from "./sections/Projects";
-import Experience from "./sections/Experience";
-import Blog from "./sections/Blog";
-import Contact from "./sections/Contact";
-
 function App() {
-  return (
-    <div className="bg-gray-100 min-h-screen flex flex-col">
-      <Navbar />
-      <ScrollToTop />
-      <main className="flex-1 flex flex-col gap-0">
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Experience />
-        <Blog />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <div>Portfolio</div>;
 }
 
 export default App;
