14483f8e0ba32360d392ee5f9f63f14e2497af9c
vasudevshetty <vasudeepu2815@gmail.com>
2025-07-22 22:39:15 +0530

    Remove outdated data files and utility functions; streamline project structure by deleting personal, project, and data index files, along with associated CSS and utility functions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import { HelmetProvider } from "react-helmet-async";
-import Hero from "./components/sections/Hero";
-import About from "./components/sections/About";
-import Skills from "./components/sections/Skills";
-import Experience from "./components/sections/Experience";
-import Projects from "./components/sections/Projects";
-import Services from "./components/sections/Services";
-import Contact from "./components/sections/Contact";
-
 function App() {
   return (
-    <HelmetProvider>
-      <div className="min-h-screen bg-bg-primary text-text-primary">
-        {/* Hero Section */}
-        <Hero />
-
-        {/* About Section */}
-        <About />
-
-        {/* Skills Section */}
-        <Skills />
-
-        {/* Experience Section */}
-        <Experience />
-
-        {/* Projects Section */}
-        <Projects />
-
-        {/* Services Section */}
-        <Services />
-
-        {/* Contact Section */}
-        <Contact />
-      </div>
-    </HelmetProvider>
-  );
+    <div>
+      hello world 
+    </div>
+  )
 }
 
-export default App;
+export default App
